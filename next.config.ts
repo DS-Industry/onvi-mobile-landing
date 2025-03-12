7d522f845b0fb7112920f8b955a6194c1584ccdd
devang-garg <devang@moy-ka.com>
2025-03-12 16:12:22 +0530

    Added the footer section in the landing page.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // Required for static exports
+  },
+  basePath: "/onvi-mobile-landing",
+  assetPrefix: "/onvi-mobile-landing",
 };
 
 export default nextConfig;
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,12 +1,20 @@
+"use client";
+
 import Image from "next/image";
-import Link from "next/link";
 import RunningImage from "@images/OnviLandingBg.jpg";
 import Mobile1 from "@images/Mobile1.jpg";
 import Mobile2 from "@images/Mobile2.jpg";
 import Mobile3 from "@images/Mobile3.jpg";
 import Mobile4 from "@images/Mobile4.jpg";
+import { useRef } from "react";
 
 export default function LandingPage() {
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollToBottom = () => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className="w-full">
       {/* Hero Section */}
@@ -30,11 +38,9 @@ export default function LandingPage() {
               Удобное приложение для поиска моек самообслуживания рядом с вами.
               Оплата услуг, кешбэк и более 80 партнёров в 10 регионах России.
             </p>
-            <Link href="#">
-              <button className="bg-white text-black px-6 py-3 rounded-full text-lg font-bold hover:bg-gray-200 transition">
-                Скачать приложение
-              </button>
-            </Link>
+            <button className="bg-white cursor-pointer text-black px-6 py-3 rounded-full text-lg font-bold hover:bg-gray-200 transition" onClick={scrollToBottom}>
+              Скачать приложение
+            </button>
           </div>
 
           {/* Right Section */}
@@ -53,7 +59,7 @@ export default function LandingPage() {
       </div>
 
       {/* Mobile App Screenshots */}
-      <div className="flex flex-nowrap justify-center gap-4 sm:gap-6 px-4 sm:px-6 md:px-10 overflow-hidden">
+      <div className="flex flex-nowrap justify-center gap-4 sm:gap-6 px-4 sm:px-6 md:px-10 overflow-hidden mb-10">
         <Image
           src={Mobile1}
           alt="Mobile 1"
@@ -74,6 +80,30 @@ export default function LandingPage() {
           alt="Mobile 4"
           className="h-64 w-[40%] sm:h-72 sm:w-[22%] md:h-80 md:w-[18%] lg:h-96 lg:w-[15%] transition-all duration-300"
         />
+      </div>
+      <div ref={bottomRef} className="relative min-h-screen bg-[#1e1e1e] text-white text-center px-4">
+        <div className="flex flex-col items-center mb-20">
+          <h1 className="text-3xl font-semibold mt-40 mb-4">ONVI: ваш надёжный помощник в уходе за авто</h1>
+          <p className="text-sm mb-4 text-[#afafaf]">Приложение Onvi доступно бесплатно в App Store и Google Play.</p>
+
+          <div className="flex gap-4 mb-8">
+            <a href="#" className="flex items-center">
+              <Image src={Mobile1} alt="Download on the App Store" className="h-12 w-12" />
+            </a>
+            <a href="#" className="flex items-center">
+              <Image src={Mobile2} alt="Get it on Google Play" className="h-12 w-12" />
+            </a>
+          </div>
+
+          <h2 className="text-4xl font-semibold mb-2">Наши контакты:</h2>
+          <p className="text-2xl">Телефон поддержки: +7(800)250-20-19</p>
+          <p className="text-2xl mb-4">Почта поддержки: support@onvione.ru</p>
+          <a href="https://docs.google.com/document/d/1H5DFxDJfFBxK6wNK3iIydC9Qp1zaQsuSxZkjaPcCVyc/edit?tab=t.0" target="_blank" rel="noreferrer noopener" className="text-[#ff8652]">
+            Политика конфиденциальности
+          </a>
+        </div>
+        <hr className="bg-white" />
+        <div className="text-xs ml-10 mt-10 text-start font-semibold">© &quot;Onvione&quot; 2024 Все права защищены.</div>
       </div>
     </div>
   );
