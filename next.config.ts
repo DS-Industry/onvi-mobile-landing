0b5640298756807fbdacba8007ca96a7c42182a1
devang-garg <devang@moy-ka.com>
2025-03-13 13:20:55 +0530

    changed next config ts file.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig: import('next').NextConfig = {
   output: "export",
-  basePath: "/onvi-landing-page", // Change this to your repo name
+  basePath: "/onvi-landing-page", // Change this to your GitHub repo name
   assetPrefix: "/onvi-landing-page/",
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 module.exports = nextConfig;
