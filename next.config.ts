a91d3913c4bfefac49e87cda68a3e31534c082a2
adamsbear34 <es1230200306@gmail.com>
2025-03-26 16:36:30 +0300

    fixed config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 const nextConfig: import('next').NextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  basePath: "/onvi-mobile-landing", // Your repo name (case-sensitive!)
-  assetPrefix: "/onvi-mobile-landing/",
-  output: 'export'
+
 };
 
 export default nextConfig;
